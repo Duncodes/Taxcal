4cd74e1915ab012744e4bfc653094c3c59fef426
duncan <duncangitonga292@gmail.com>
2016-06-26 21:16:05 +0300

    First commit

---
+++ Tax/MainWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+using Tax;
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel){
+		Build ();
+
+		find.Clicked += new EventHandler (OnButtonClicked);
+
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a){
+		Application.Quit ();
+		a.RetVal = true;
+	}
+	void OnButtonClicked (object obj, EventArgs args){
+		float  GrossAmmout= float.Parse(data.Text);
+		//TaxValue.Text="Hi";
+		TaxMethod tax =new  TaxMethod();
+		TaxValue.Text=tax.procedure (GrossAmmout);
+		}
+		}
+
+	
+
+		
+
---
+++ Tax/TaxMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Gtk;
+namespace Tax
+{
+	public class TaxMethod
+	{
+		
+		public TaxMethod ()
+		{
+			
+		}
+		public String procedure(float GrossAmmout)
+		{
+			Label taxValue = new Label ();
+			if (GrossAmmout < 0)
+				return taxValue.Text = "The Ammout is not taxable";
+			else if (GrossAmmout <= 10165)
+				return taxValue.Text = (1016).ToString ();
+			else if (GrossAmmout <= 19741)
+				return taxValue.Text = ((1016 + 1436).ToString ());
+			else if (GrossAmmout <= 29317)
+				return taxValue.Text = ((2452 + 1915).ToString ());
+			else if (GrossAmmout <= 38893)
+				return taxValue.Text = ((4367 + 2394).ToString ());
+			else if (GrossAmmout > 38893)
+				return taxValue.Text = ((6761 + ((4367 + 2394) * 0.3)).ToString ());
+			else {
+				Console.WriteLine ();
+				return taxValue.Text = "The Ammout is not taxable";
+		}
+	}
+}
+
+}
